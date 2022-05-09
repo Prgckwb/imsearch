f6bcae5e6bd90992e98c2ee118973720e9663212
Prgckwb <mr.yamaguti@gmail.com>
2022-05-09 18:43:59 +0900

    add

--- features.py
+++ features.py
@@ -2,13 +2,13 @@ import glob
 
 import cv2
 import numpy as np
+from tqdm import tqdm
 
 IMAGE_DIR = "static/images/ramen"
 images_list = [img for img in sorted(glob.glob(f"{IMAGE_DIR}/*.jpg"))]
 
 
 def rgb_hist(img_path):
-    # img = cv2.imread("./static/images/ramen/2192.jpg")
     img = cv2.imread(img_path)
     b, g, r = img[..., 0], img[..., 1], img[..., 2]
     img_shape = img.shape[0] * img.shape[1]
@@ -20,20 +20,19 @@ def rgb_hist(img_path):
     hist = np.array([hist_r, hist_g, hist_b])
     return hist
 
-
-def write_feature_data(data, file_name, data_dir="static/data"):
-    np.save(f"{data_dir}/{file_name}", data)
-
-
+#
 def write_alldata(images_list, method, filename, data_dir="static/data"):
-    all_data = np.empty((len(images_list),))
+    all_data = []
 
-    for i, image in enumerate(images_list):
+    for i, image in enumerate(tqdm(images_list)):
         data = method(image)
-        all_data[i] = data
+        all_data.append(data)
 
+    all_data = np.array(all_data)
     np.save(f"{data_dir}/{filename}", all_data)
 
 
 if __name__ == '__main__':
     write_alldata(images_list, rgb_hist, "sample")
+    d = np.load("static/data/sample.npy")
+    print(d.shape)
