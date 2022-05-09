e6996e6a7cb4963a51f2d8cb9c92263431045701
Prgckwb <ckwb.uec@gmail.com>
2022-05-09 17:15:40 +0900

    add

--- features.py
+++ features.py
@@ -1,4 +1,12 @@
 import cv2
-import numpy as np
+
 
 def calc():
+    img = cv2.imread("./static/images/ramen/2192.jpg")
+    b, g, r = img[..., 0], img[...,1], img[..., 2]
+    hist_b = cv2.calcHist([b], [0], None, [256], [0, 256])
+    print(hist_b)
+
+
+if __name__ == '__main__':
+    calc()
