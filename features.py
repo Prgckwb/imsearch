a3ecb5cfffc86c087e5ef5c3b5db8f3abbd40489
Prgckwb <mr.yamaguti@gmail.com>
2022-05-09 18:04:13 +0900

    add

--- features.py
+++ features.py
@@ -1,18 +1,22 @@
 import cv2
 
 
-def rgb_hist(img):
+def rgb_hist(img_path):
     # img = cv2.imread("./static/images/ramen/2192.jpg")
+    img = cv2.imread(img_path)
     b, g, r = img[..., 0], img[..., 1], img[..., 2]
-    print(img.shape)
-    hist_b = cv2.calcHist([b], [0], None, [256], [0, 256])
-    hist_g = cv2.calcHist([g], [0], None, [256], [0, 256])
-    hist_r = cv2.calcHist([r], [0], None, [256], [0, 256])
+    img_shape = img.shape[0] * img.shape[1]
+
+    hist_b = cv2.calcHist([b], [0], None, [256], [0, 256]) / img_shape
+    hist_g = cv2.calcHist([g], [0], None, [256], [0, 256]) / img_shape
+    hist_r = cv2.calcHist([r], [0], None, [256], [0, 256]) / img_shape
 
     hist = [hist_r, hist_g, hist_b]
     return hist
 
 
 if __name__ == '__main__':
-    # calc()
-    pass
+    img_path = "static/images/ramen/2192.jpg"
+    h = rgb_hist(img_path)
+    print(h[0])
+    print(h[0].sum())
