8daebbb4161af50cfe2f5018d07e1b4fbd2e2705
Prgckwb <ckwb.uec@gmail.com>
2022-05-09 17:54:56 +0900

    add

--- features.py
+++ features.py
@@ -1,12 +1,17 @@
 import cv2
 
 
-def calc():
-    img = cv2.imread("./static/images/ramen/2192.jpg")
-    b, g, r = img[..., 0], img[...,1], img[..., 2]
+def rgb_hist(img):
+    # img = cv2.imread("./static/images/ramen/2192.jpg")
+    b, g, r = img[..., 0], img[..., 1], img[..., 2]
     hist_b = cv2.calcHist([b], [0], None, [256], [0, 256])
-    print(hist_b)
+    hist_g = cv2.calcHist([g], [0], None, [256], [0, 256])
+    hist_r = cv2.calcHist([r], [0], None, [256], [0, 256])
+
+    hist = [hist_r, hist_g, hist_b]
+    return hist
 
 
 if __name__ == '__main__':
-    calc()
+    # calc()
+    pass
