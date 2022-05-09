ceaba1d877f62bedc385b814beba73b6e4549961
Prgckwb <ckwb.uec@gmail.com>
2022-05-09 15:49:51 +0900

    add

--- template/main_page.py
+++ template/main_page.py
@@ -1,34 +1,7 @@
 # HTML情報を返す
 def get_page(image, feature, images_list):
-    # 画像テーブルのサイズ
-    table_w = 7
-    table_h = 10
-
-    # 画像のサイズ
-    image_w = 200
-    image_h = 100
-
-    images_table = ""
-
-    for i in range(table_h):
-
-        # 画像テーブルの列を作成
-        # language=HTML
-        images_table += "<tr>"
-
-        for j in range(table_w):
-
-            # 画像テーブルの列に対して画像をtable_wの分作成
-            # language=HTML
-            images_table += f'''
-                    <td><a href="./?image={table_w * i + j}&feature={feature}">
-                        <img src="{images_list[table_w * i + j]}" alt="hoge" 
-                        style="width: {image_w}px; height:{image_h}px; object-fit: cover">
-                    </a></td>
-                    '''
-
-        # language=HTML
-        images_table += "</tr>"
+    images_table = create_images_table(image, feature, images_list)
+    feature_options = create_feature_options(image)
 
     # language=HTML
     html_text = f'''
@@ -60,26 +33,8 @@ def get_page(image, feature, images_list):
                     </div>
 
                     <!-- 特徴量切り替え -->
-                    <div class="feature-options menu">
-                        <p class="title is-4">Feature Options</p>
-
-                        <div class="intersec-option">
-                            <p class="menu-label">Intersection</p>
-                            <ul class="menu-list">
-                                <li><a href="?image={image}&feature=rgb1i">RGB Color Histogram 1×1</a></li>
-                                <li><a href="?image={image}&feature=rgb2i">RGB Color Histogram 2×2</a></li>
-                                <li><a href="?image={image}&feature=rgb3i">RGB Color Histogram 3×3</a></li>
-
-                                <li><a href="?image={image}&feature=hsv1i">HSV Color Histogram 1×1</a></li>
-                                <li><a href="?image={image}&feature=hsv2i">HSV Color Histogram 2×2</a></li>
-                                <li><a href="?image={image}&feature=hsv3i">HSV Color Histogram 3×3</a></li>
-
-                                <li><a href="?image={image}&feature=luv1i">LUV Color Histogram 1×1</a></li>
-                                <li><a href="?image={image}&feature=luv2i">LUV Color Histogram 2×2</a></li>
-                                <li><a href="?image={image}&feature=luv3i">LUV Color Histogram 3×3</a></li>
-                            </ul>
-                        </div>
-                    </div>
+                    {feature_options}
+                    
                 </div>
 
                 <!-- 画像のテーブル-->
@@ -96,3 +51,63 @@ def get_page(image, feature, images_list):
     </html>
     '''
     return html_text
+
+
+# 画像一覧テーブルを作成する
+def create_images_table(image, feature, images_list):
+    # 画像テーブルのサイズ
+    table_w = 7
+    table_h = 10
+
+    # 画像のサイズ
+    image_w = 200
+    image_h = 100
+
+    images_table = ""
+
+    for i in range(table_h):
+
+        # 画像テーブルの列を作成
+        # language=HTML
+        images_table += "<tr>"
+
+        for j in range(table_w):
+            # 画像テーブルの列に対して画像をtable_wの分作成
+            # language=HTML
+            images_table += f'''
+                       <td><a href="./?image={table_w * i + j}&feature={feature}">
+                           <img src="{images_list[table_w * i + j]}" alt="hoge" 
+                           style="width: {image_w}px; height:{image_h}px; object-fit: cover">
+                       </a></td>
+                       '''
+
+        # language=HTML
+        images_table += "</tr>"
+    return images_table
+
+
+def create_feature_options(image):
+    # language=HTML
+    feature_options = '''
+    <div class="feature-options menu">
+        <p class="title is-4">Feature Options</p>
+
+        <div class="intersec-option">
+            <p class="menu-label">Intersection</p>
+            <ul class="menu-list">
+                <li><a href="?image={image}&feature=rgb1i">RGB Color Histogram 1×1</a></li>
+                <li><a href="?image={image}&feature=rgb2i">RGB Color Histogram 2×2</a></li>
+                <li><a href="?image={image}&feature=rgb3i">RGB Color Histogram 3×3</a></li>
+
+                <li><a href="?image={image}&feature=hsv1i">HSV Color Histogram 1×1</a></li>
+                <li><a href="?image={image}&feature=hsv2i">HSV Color Histogram 2×2</a></li>
+                <li><a href="?image={image}&feature=hsv3i">HSV Color Histogram 3×3</a></li>
+
+                <li><a href="?image={image}&feature=luv1i">LUV Color Histogram 1×1</a></li>
+                <li><a href="?image={image}&feature=luv2i">LUV Color Histogram 2×2</a></li>
+                <li><a href="?image={image}&feature=luv3i">LUV Color Histogram 3×3</a></li>
+            </ul>
+        </div>
+    </div>
+    '''
+    return feature_options
