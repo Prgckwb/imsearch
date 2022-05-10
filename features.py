3542a7ba5727f826087684defe6e118b441d0478
Prgckwb <ckwb.uec@gmail.com>
2022-05-10 14:26:40 +0900

    Refactor

--- features.py
+++ features.py
@@ -3,31 +3,40 @@ import glob
 import cv2
 import numpy as np
 from tqdm import tqdm
-from util import IMAGE_DIR, DATA_DIR
 
-images_list = [img for img in sorted(glob.glob(f"{IMAGE_DIR}/*.jpg"))]
+from util import IMAGE_DIR
+
+images_path_list = [img for img in sorted(glob.glob(f"{IMAGE_DIR}/*.jpg"))]
 
 
 # RGBヒストグラムによる特徴量抽出を行う
-def rgb_hist(img_path):
+def extract_hist(img_path, hist_type="RGB"):
     img = cv2.imread(img_path)
-    b, g, r = img[..., 0], img[..., 1], img[..., 2]
+
+    if hist_type == "RGB":
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    elif hist_type == "HSV":
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    elif hist_type == "LUV":
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2LUV)
+
+    d1, d2, d3 = img[..., 0], img[..., 1], img[..., 2]
     img_shape = img.shape[0] * img.shape[1]
 
-    hist_b = cv2.calcHist([b], [0], None, [256], [0, 256]) / img_shape
-    hist_g = cv2.calcHist([g], [0], None, [256], [0, 256]) / img_shape
-    hist_r = cv2.calcHist([r], [0], None, [256], [0, 256]) / img_shape
+    hist_d1 = cv2.calcHist([d3], [0], None, [256], [0, 256]) / img_shape
+    hist_d2 = cv2.calcHist([d2], [0], None, [256], [0, 256]) / img_shape
+    hist_d3 = cv2.calcHist([d1], [0], None, [256], [0, 256]) / img_shape
 
-    hist = np.array([hist_r, hist_g, hist_b])
+    hist = np.array([hist_d1, hist_d2, hist_d3])
     return hist
 
 
 # 特徴抽出methodを定義して全画像リストに対して特徴抽出
-def write_alldata(images_list, method, filename, data_dir="static/data"):
+def write_alldata(method, filename, data_type, data_dir="static/data"):
     all_data = []
 
-    for i, image in enumerate(tqdm(images_list)):
-        data = method(image)
+    for i, image in enumerate(tqdm(images_path_list)):
+        data = method(image, data_type)
         all_data.append(data)
 
     all_data = np.array(all_data)
@@ -48,5 +57,12 @@ def compare_hist(data, query_index, images_list):
     return similarity
 
 
+# 特徴量データの作成
+def create_features_data():
+    write_alldata(extract_hist, "RGB1", "RGB")
+    write_alldata(extract_hist, "HSV1", "HSV")
+    write_alldata(extract_hist, "LUV1", "LUV")
+
+
 if __name__ == '__main__':
-    data = np.load("static/data/RGB1.npy")
+    create_features_data()
