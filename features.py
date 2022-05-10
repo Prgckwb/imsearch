ac31151991366e447dedc7ed4070113d359462f4
Prgckwb <ckwb.uec@gmail.com>
2022-05-10 17:01:00 +0900

    Refactor

--- features.py
+++ features.py
@@ -31,12 +31,38 @@ def extract_hist(img_path, hist_type="RGB"):
     return hist
 
 
+def extract_hist2(img_path, hist_type="RGB", split_n=1):
+    img = cv2.imread(img_path)
+
+    if hist_type == "RGB":
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    elif hist_type == "HSV":
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    elif hist_type == "LUV":
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2LUV)
+
+    images = split_image(img, split_n)
+    hist = []
+
+    for image in images:
+        d1, d2, d3 = image[..., 0], image[..., 1], image[..., 2]
+        img_shape = image.shape[0] * image.shape[1]
+
+        hist_d1 = cv2.calcHist([d3], [0], None, [256], [0, 256]) / img_shape
+        hist_d2 = cv2.calcHist([d2], [0], None, [256], [0, 256]) / img_shape
+        hist_d3 = cv2.calcHist([d1], [0], None, [256], [0, 256]) / img_shape
+
+        h = np.array(hist_d1, hist_d2, hist_d3)
+        hist.append(h)
+    return hist
+
+
 # 特徴抽出methodを定義して全画像リストに対して特徴抽出
-def write_alldata(method, filename, data_type, data_dir="static/data"):
+def write_alldata(filename, data_type, data_dir="static/data"):
     all_data = []
 
     for i, image in enumerate(tqdm(images_path_list)):
-        data = method(image, data_type)
+        data = extract_hist(image, data_type)
         all_data.append(data)
 
     all_data = np.array(all_data)
@@ -45,11 +71,21 @@ def write_alldata(method, filename, data_type, data_dir="static/data"):
     np.save(f"{data_dir}/{filename}", all_data)
 
 
+def write_alldata2(filename, data_type, data_dir="static/data"):
+    all_data = []
+
+    for i, image in enumerate(tqdm(images_path_list)):
+        data = extract_hist2(image, data_type, 2)
+        all_data.append(data)
+
+    all_data = np.array(all_data)
+    print(all_data)
+
 # 特徴量データの作成
 def create_features_data():
-    write_alldata(extract_hist, "RGB1", "RGB")
-    write_alldata(extract_hist, "HSV1", "HSV")
-    write_alldata(extract_hist, "LUV1", "LUV")
+    write_alldata("RGB1", "RGB")
+    write_alldata("HSV1", "HSV")
+    write_alldata("LUV1", "LUV")
 
 
 def split_image(img, n):
@@ -66,4 +102,4 @@ def split_image(img, n):
 
 
 if __name__ == '__main__':
-    pass
+    write_alldata2("RGB2", "RGB")
