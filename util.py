e3b2993bca6449819b0c7e0d9625b2975f426581
Prgckwb <ckwb.uec@gmail.com>
2022-05-13 18:15:46 +0900

    Refactor

--- util.py
+++ util.py
@@ -152,8 +152,6 @@ def compare_hist(data, query_index, feature, images_list):
             # DCNNの時
             for i in range(len(images_list)):
                 s = np.minimum(data[query_index][0], data[i][0])
-                s = s / np.sum(np.sqrt(s**2))
-                # similarity.append(s.sum())
                 similarity.append(s.sum())
     return similarity
 
