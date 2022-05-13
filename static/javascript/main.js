da1aa3295fae4ca65b2e99febcc37bfe5ec75d11
Prgckwb <ckwb.uec@gmail.com>
2022-05-13 14:37:55 +0900

    Refactor

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -14,9 +14,19 @@ function getUrlQueries() {
     return queries
 }
 
+function isEmpty(obj) {
+    return !Object.keys(obj).length;
+}
+
 // クエリ情報を吐き出す
 function activateFeatureOption() {
     let queries = getUrlQueries()
-    let feature_option = document.getElementById(queries['f'])
-    feature_option.classList.add("is-active")
+    if (isEmpty(queries)) {
+        let default_feature = document.getElementById("10")
+        default_feature.classList.add("is-active")
+    } else {
+        let feature_option = document.getElementById(query_f)
+        feature_option.classList.add("is-active")
+    }
+
 }
